a610b695091dce10a1f890106f22d8bf0c50de68
enzozafra <zafra@ualberta.ca>
2017-11-28 11:14:10 -0700

    a4vmsim

---
+++ Assignments/a4/a4vmsim.c
@@ -0,0 +1,15 @@
+/**
+  *
+  * a4vmsim.c
+  *
+  * Lorenzo Zafra 1395521
+  * CMPUT 379 Assignment 4
+  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "stack.h"
+#include "a4vmsim.h"
+
---
+++ Assignments/a4/a4vmsim.h
@@ -0,0 +1,14 @@
+/**
+  *
+  * a4vmsim.h
+  *
+  * Lorenzo Zafra 1395521
+  * CMPUT 379 Assignment 4
+  */
+
+
+#ifndef a4vmsim_h
+#define a4vmsim_h
+
+
+#endif
